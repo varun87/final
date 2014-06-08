c96823869854b9407d7b98f325da818235039e2e
vagrant <vagrant@kellogg-dev-box>
2014-06-08 05:07:42 +0000

    my first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530122303) do
+ActiveRecord::Schema.define(version: 20140608044943) do
 
   create_table "apartmenttypes", force: true do |t|
     t.string  "title"
